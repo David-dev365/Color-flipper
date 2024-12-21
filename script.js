2e215903a6cff9eae7d54629cb01a7f6104d9f57
David-dev365 <Daviditodo08@gmail.com>
2024-12-21 15:52:12 +0100

    created the randomColor function

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ const body = document.getElementsByTagName("body")[0];
 function setColor(bColor){
     body.style.backgroundColor = bColor
 }
+
+function randomColor(){
+    const red = Math.round(Math.random() * 225) 
+    const green = Math.round(Math.random() * 225) 
+    const blue = Math.round(Math.random() * 225) 
+
+    const color = `rgb(${red}, ${green}, ${blue})`
+    body.style.backgroundColor = color
+    document.getElementById("show").innerHTML = `string:${red}`
+
+    // return {red}
+}
+
+// const {red} = randomColor()
+// document.getElementById("show").innerHTML = `string:${red}`
+
+randomColor();
